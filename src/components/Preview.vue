3c36b5fe1ac5cd0cf81448909b40f9850b197572
Warathaya Rattachai <warathaya.nursewarathaya@mail.kmutt.ac.th>
2021-04-15 00:20:16 +0700

    [Create]Project,layout of web

---
+++ src/components/Preview.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="preview">
+    <div class="w-96 h-96 mx-36 my-24">
+      <img src="../assets/bbt/bbt-1.png" alt="">
+    </div>
+    
+  </div >
+</template>
+
+<script>
+export default {
+  name: "Preview"
+};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    theme: {
+        colors: {
+            darkBlue: '#003049',
+            tea:'#e5bda0',
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: colors.coolGray,
+            red: colors.red,
+            yellow: colors.amber,
+            blue: colors.blue
+
+
+        }
+
+    }
+
+}
